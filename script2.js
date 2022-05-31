c6ae966dc92be3ea41e7c4cf2efc1e4736cd8e70
KarolBlandon <101797673+KarolBlandon@users.noreply.github.com>
2022-06-01 00:22:52 +0200

    cambios de js

---
+++ script2.js
@@ -0,0 +1,24 @@
+function perimetro(radio) {
+return 2 * Math.PI*radio;
+}
+console.log(perimetro(6));
+    
+function area(radio){
+return Math.PI*radio*radio;
+ }
+console.log(area(6));
+    
+function perimetro(lado){
+return 4 * lado;
+}
+console.log(perimetro(7));
+    
+function area(lado){
+return lado*lado;
+}
+console.log(area (3));
+    
+function hipotenusa (b,c){
+return Math.hypot (b,c);
+}
+console.log (hipotenusa(30, 5));
